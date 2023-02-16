96ea2805d65fd4463032e5243d307b81150c83d4
diogorenam <diogorenan006@gmail.com>
2023-02-16 17:51:39 -0300

    first commit

--- App.js
+++ App.js
@@ -1,10 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Title from './src/components/Title';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Title />
+      <Text>Olá Diogo! 
+      Como posso</Text>
+
       <StatusBar style="auto" />
     </View>
   );
